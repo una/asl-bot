442683cd80d403b331320cf5c66a80d585adc2d4
Una Kravets <una.kravets@gmail.com>
2017-09-25 15:18:22 -0400

    added code comments

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Settin gup server port
 app.set('port', (process.env.PORT || 9001));
 
+// Sanity check to see the app running on proper port
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+// Core of the functionality here
 app.post('/post', function(req, res){
   let searchQuery = req.body.text;
 
+  // Yes, this is a public API key from Giphy
+  // Not great code here but its literally just doing a single search
   request(`http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=sign%20with%20robert%20${searchQuery}`, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       // Data transformations here
@@ -31,6 +36,7 @@ app.post('/post', function(req, res){
   });
 });
 
+// Listen on the port for the app to run
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
